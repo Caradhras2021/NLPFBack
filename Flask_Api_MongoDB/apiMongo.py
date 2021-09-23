13e384ff7640464bdc3c257faf84465554cadfcc
julien travers <julien.travers@epita.fr>
2021-09-23 19:56:42 +0200

    add(apiMongo) init

---
+++ Flask_Api_MongoDB/apiMongo.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_mongoengine import MongoEngine
+from api_constants import mongodb_password
+
+app = Flask(__name__)
+databaseName = "APInlpf"
+databaseURI = 'mongodb+srv://caradhras:{}@apinlpf.elufi.mongodb.net/{}?retryWrites=true&w=majority'.format(mongodb_password, databaseName)
+app.config["MONGO_HOST"] = databaseURI
+db = MongoEngine()
+db.init_app(app)
+
+if __name__ == '__main__':
+    app.run() 
