b1e34c13918d51a13190876d68b6c2598cae9178
Yann Nitzpon <ynitzpon@gmail.com>
2021-10-11 15:23:07 +0200

    filter by surface range WORKS

--- src/models/transactionModels.py
+++ src/models/transactionModels.py
@@ -5,8 +5,11 @@ mandatoryFields = { "id_mutation": 1, "date_mutation": 1, "valeur_fonciere": 1, 
      "adresse_nom_voie": 1, "code_postal": 1, "nom_commune": 1, "longitude": 1, "latitude": 1, "surface_terrain": 1,
      "type_local": 1, "nombre_pieces_principales": 1, "lot1_surface_carrez": 1 }
 
-def transactionModel(filters, pageNumber=1, pageSize=20):
-    query = { "$and": filters }
+def transactionModel(filters, surface, pageNumber=1, pageSize=20):
+    if surface >= 0:
+        query = { "$and": filters, "lot1_surface_carrez": { "$gte": surface - 10, "$lte": surface + 10} }
+    else:
+        query = { "$and": filters }
 
     if pageNumber == 0:
         pageNumber = 1
@@ -21,4 +24,5 @@ def transactionModel(filters, pageNumber=1, pageSize=20):
         doc.pop('_id')
         res.append(doc)
     response = json.dumps(res)
+    
     return response
--- src/services/transactionService.py
+++ src/services/transactionService.py
@@ -1,9 +1,23 @@
 from models.transactionModels import transactionModel
 
-def transactionService(filters, pageNumber, pageSize):
+def transactionServiceSurface(filters, surface, pageNumber, pageSize):
     arrayFilters = []
     for key, value in filters.items():
         if (str(value) != ""):
             arrayFilters.append({key: value})
     transactions = transactionModel(arrayFilters, pageNumber, pageSize)
+    return transactions
+
+def transactionService(filters, pageNumber, pageSize):
+    arrayFilters = []
+    surface = -1
+    for key, value in filters.items():
+        if (str(key) == "lot1_surface_carrez" and str(value) != ""):
+            surface = value
+            continue
+        if (str(value) != ""):
+            arrayFilters.append({key: value})
+
+    transactions = transactionModel(arrayFilters, surface, pageNumber, pageSize)
+
     return transactions
